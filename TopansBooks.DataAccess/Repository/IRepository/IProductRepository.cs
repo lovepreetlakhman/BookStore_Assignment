a77db553196810a6d5ad4084b022ac55fe81e67e
topandas69 <W0790712@myscc.ca>
2022-11-24 21:47:26 -0500

    created ProductRepository interface and its class implementing it

---
+++ TopansBooks.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TopansBooks.Models;
+
+namespace TopansBooks.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product product);
+    }
+}
